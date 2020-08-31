7432d369cacb5607e461acb7c0f25e00d1f1e2b2
namooplus <km1059ks@gmail.com>
2020-08-31 21:55:18 +0900

    Major Update : 4.0.1 release

--- app/src/main/java/nm/security/namooprotector/receiver/UpdateReceiver.kt
+++ app/src/main/java/nm/security/namooprotector/receiver/UpdateReceiver.kt
@@ -3,9 +3,10 @@ package nm.security.namooprotector.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import nm.security.namooprotector.R
 import nm.security.namooprotector.util.CheckUtil
+import nm.security.namooprotector.util.ResourceUtil
 import nm.security.namooprotector.util.ServiceUtil
 
 class UpdateReceiver : BroadcastReceiver()
@@ -14,5 +15,7 @@ class UpdateReceiver : BroadcastReceiver()
     {
         if (CheckUtil.isNPValid && CheckUtil.isServiceRunning)
             ServiceUtil.runService(true)
+
+        Toast.makeText(context, ResourceUtil.getString(R.string.alert_np_updated), Toast.LENGTH_LONG).show()
     }
 }
